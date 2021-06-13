a40d42d802e544296922e128495c9af829813d9b
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-06-13 09:23:41 -0400

    Basic face elements

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
-function changeColor() {
-    document.getElementById("path-head").style.fill = "#bd5524";
+//facial features are named left-right from the animation's perspective
+const skinTones = ["#c58c85","#ecbcb4","#d1a3a4","#a1665e","#4C3435","#592f2a"]
+const skinElements = document.getElementsByClassName("skin");
+let skinTone;
+let skinToneReference = getRandomInt(skinTones.length);
+
+changeSkinColor(); 
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function changeSkinColor() {
+    skinToneReference = skinToneReference===(skinTones.length-1) ? 0 : skinToneReference+1;
+    skinTone = skinTones[skinToneReference];
+    for (let i=0;i<skinElements.length;i++) {
+        skinElements[i].style.fill = skinTone;
+    }
+}
+
+function changeNose() {
+    document.getElementById("nose").style.display= "none";
 }
